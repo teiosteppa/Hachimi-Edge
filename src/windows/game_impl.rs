35b13181c1287c470e84a21f44a4892c44ac4417
Meewko <82271768+Kamirias@users.noreply.github.com>
2025-09-26 04:50:52 +0700

    change client checking method

--- src/windows/game_impl.rs
+++ src/windows/game_impl.rs
@@ -33,17 +33,13 @@ pub fn get_data_dir(package_name: &str) -> PathBuf {
         .join("hachimi")
 }
 
+
 pub fn is_steam_release(package_name: &str) -> bool {
     let exec_path = Path::new(package_name);
-    let exec_dir = exec_path.parent().unwrap();
-
-    let mut data_dir_name = exec_path.file_stem().unwrap().to_owned();
-    data_dir_name.push("_Data");
-
-    let mut steam_api_dll = exec_dir.join(data_dir_name);
-    steam_api_dll.push("Plugins");
-    steam_api_dll.push("x86_64");
-    steam_api_dll.push("steam_api64.dll");
-
-    steam_api_dll.metadata().is_ok_and(|m| m.is_file())
+    let is_jp_steam = exec_path
+        .file_stem()
+        .and_then(|s| s.to_str())
+        .map(|s| s.eq_ignore_ascii_case("umamusumeprettyderby_jpn"))
+        .unwrap_or(false);
+    is_jp_steam
 }
