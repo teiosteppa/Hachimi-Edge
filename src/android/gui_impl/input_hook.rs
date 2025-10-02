3e14890da20768e2a77250f0d927f4e28d78ba36
kairusds <32636532+kairusds@users.noreply.github.com>
2025-10-02 19:06:42 +0800

    Fix android touch with GUI enabled

--- src/android/gui_impl/input_hook.rs
+++ src/android/gui_impl/input_hook.rs
@@ -36,11 +36,6 @@ extern "C" fn nativeInjectEvent(mut env: JNIEnv, obj: JObject, input_event: JObj
     let key_event_class = env.find_class("android/view/KeyEvent").unwrap();
 
     if env.is_instance_of(&input_event, &motion_event_class).unwrap() {
-        // early return using atomic check to prevent mutex lock overhead
-        if !Gui::is_consuming_input_atomic() {
-            return get_orig_fn!(nativeInjectEvent, NativeInjectEventFn)(env, obj, input_event);
-        }
-
         let Some(mut gui) = Gui::instance().map(|m| m.lock().unwrap()) else {
             return get_orig_fn!(nativeInjectEvent, NativeInjectEventFn)(env, obj, input_event);
         };
@@ -50,7 +45,10 @@ extern "C" fn nativeInjectEvent(mut env: JNIEnv, obj: JObject, input_event: JObj
         let action_masked = action & ACTION_MASK;
         let pointer_index = (action & ACTION_POINTER_INDEX_MASK) >> ACTION_POINTER_INDEX_SHIFT;
 
-        if pointer_index != 0 {
+        // hmmmmm
+        if !Gui::is_consuming_input_atomic() {
+            return get_orig_fn!(nativeInjectEvent, NativeInjectEventFn)(env, obj, input_event);
+        } else if pointer_index != 0 && Gui::is_consuming_input_atomic() {
             return JNI_TRUE;
         }
 
