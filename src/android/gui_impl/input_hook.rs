f795f4ff880804a6e5e8c2ae593f8cd40680ae70
kairusds <32636532+kairusds@users.noreply.github.com>
2025-10-02 19:15:21 +0800

    Debug fix android touch (9)

--- src/android/gui_impl/input_hook.rs
+++ src/android/gui_impl/input_hook.rs
@@ -35,7 +35,7 @@ extern "C" fn nativeInjectEvent(mut env: JNIEnv, obj: JObject, input_event: JObj
     let motion_event_class = env.find_class("android/view/MotionEvent").unwrap();
     let key_event_class = env.find_class("android/view/KeyEvent").unwrap();
 
-    if env.is_instance_of(&input_event, &motion_event_class).unwrap() {
+    if env.is_instance_of(&input_event, &motion_event_class).unwrap() && Gui::is_consuming_input_atomic() {
         let Some(mut gui) = Gui::instance().map(|m| m.lock().unwrap()) else {
             return get_orig_fn!(nativeInjectEvent, NativeInjectEventFn)(env, obj, input_event);
         };
@@ -45,10 +45,7 @@ extern "C" fn nativeInjectEvent(mut env: JNIEnv, obj: JObject, input_event: JObj
         let action_masked = action & ACTION_MASK;
         let pointer_index = (action & ACTION_POINTER_INDEX_MASK) >> ACTION_POINTER_INDEX_SHIFT;
 
-        // hmmmmm
-        if !Gui::is_consuming_input_atomic() {
-            return get_orig_fn!(nativeInjectEvent, NativeInjectEventFn)(env, obj, input_event);
-        } else if pointer_index != 0 && Gui::is_consuming_input_atomic() {
+        if pointer_index != 0 {
             return JNI_TRUE;
         }
 
