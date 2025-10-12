e7ef1a6717a04d39632940a387473451b25adae9
noccu <37862870+noccu@users.noreply.github.com>
2025-10-12 23:48:49 +0200

    Swap default meta to UmaTL GitLab mirror
    This should improve access for some regions without affecting others.

--- src/core/hachimi.rs
+++ src/core/hachimi.rs
@@ -318,7 +318,7 @@ impl Config {
     fn default_ui_scale() -> f32 { 1.0 }
     fn default_story_choice_auto_select_delay() -> f32 { 1.2 }
     fn default_story_tcps_multiplier() -> f32 { 3.0 }
-    fn default_meta_index_url() -> String { "https://raw.githubusercontent.com/UmaTL/hachimi-meta/main/meta.json".to_owned() }
+    fn default_meta_index_url() -> String { "https://gitlab.com/umatl/hachimi-meta/-/raw/main/meta.json".to_owned() }
     fn default_ui_animation_scale() -> f32 { 1.0 }
 }
 
