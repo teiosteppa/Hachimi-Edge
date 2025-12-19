ec4496f50b113a0289adc37eaf26290c7947f5cb
noccu <37862870+noccu@users.noreply.github.com>
2025-12-19 17:20:53 +0100

    Fix InitializeGame signature
    Applies UI scaling.

--- src/il2cpp/hook/umamusume/GameSystem.rs
+++ src/il2cpp/hook/umamusume/GameSystem.rs
@@ -1,4 +1,4 @@
-use crate::{core::Hachimi, il2cpp::{symbols::{get_method_addr, IEnumerator, SingletonLike, MoveNextFn}, types::*}};
+use crate::{core::{Hachimi, game::Region}, il2cpp::{symbols::{IEnumerator, MoveNextFn, SingletonLike, get_method_addr}, types::*}};
 
 static mut CLASS: *mut Il2CppClass = 0 as _;
 pub fn class() -> *mut Il2CppClass {
@@ -29,9 +29,7 @@ extern "C" fn InitializeGame_MoveNext(enumerator: *mut Il2CppObject) -> bool {
     moved
 }
 
-type InitializeGameFn = extern "C" fn(this: *mut Il2CppObject) -> IEnumerator;
-extern "C" fn InitializeGame(this: *mut Il2CppObject) -> IEnumerator {
-    let enumerator = get_orig_fn!(InitializeGame, InitializeGameFn)(this);
+fn InitializeGameCommon(enumerator: IEnumerator) -> IEnumerator {
     if Hachimi::instance().config.load().ui_scale == 1.0 { return enumerator; }
 
     if let Err(e) = enumerator.hook_move_next(InitializeGame_MoveNext) {
@@ -41,12 +39,29 @@ extern "C" fn InitializeGame(this: *mut Il2CppObject) -> IEnumerator {
     enumerator
 }
 
+type InitializeGameJpFn = extern "C" fn(this: *mut Il2CppObject, on_complete_initialize_ui: *mut Il2CppObject) -> IEnumerator;
+extern "C" fn InitializeGameJp(this: *mut Il2CppObject, on_complete_initialize_ui: *mut Il2CppObject) -> IEnumerator {
+    let enumerator = get_orig_fn!(InitializeGameJp, InitializeGameJpFn)(this, on_complete_initialize_ui);
+    InitializeGameCommon(enumerator)
+}
+
+type InitializeGameOtherFn = extern "C" fn(this: *mut Il2CppObject) -> IEnumerator;
+extern "C" fn InitializeGameOther(this: *mut Il2CppObject) -> IEnumerator {
+    let enumerator = get_orig_fn!(InitializeGameOther, InitializeGameOtherFn)(this);
+    InitializeGameCommon(enumerator)
+}
+
 pub fn init(umamusume: *const Il2CppImage) {
     get_class_or_return!(umamusume, Gallop, GameSystem);
 
-    let InitializeGame_addr = get_method_addr(GameSystem, c"InitializeGame", 0);
-
-    new_hook!(InitializeGame_addr, InitializeGame);
+    if Hachimi::instance().game.region == Region::Japan {
+        let InitializeGame_addr = get_method_addr(GameSystem, c"InitializeGame", 1);
+        new_hook!(InitializeGame_addr, InitializeGameJp);
+    }
+    else {
+        let InitializeGame_addr = get_method_addr(GameSystem, c"InitializeGame", 0);
+        new_hook!(InitializeGame_addr, InitializeGameOther);
+    }
 
     unsafe {
         CLASS = GameSystem;
