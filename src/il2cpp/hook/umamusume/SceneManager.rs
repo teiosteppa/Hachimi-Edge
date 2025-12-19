7565bd2308a6897081f10c7856a37a3fa4a1ccdf
noccu <37862870+noccu@users.noreply.github.com>
2025-12-19 18:18:10 +0100

    Fix ChangeView signature
    Functionality doesn't seem to actually be used currently.
    But at least it's fixed!

--- src/il2cpp/hook/umamusume/SceneManager.rs
+++ src/il2cpp/hook/umamusume/SceneManager.rs
@@ -1,34 +1,61 @@
 use std::sync::atomic::{self, AtomicBool};
 
-use crate::{il2cpp::{symbols::get_method_addr, types::*}, windows::wnd_hook};
+use crate::{core::{Hachimi, game::Region}, il2cpp::{symbols::get_method_addr, types::*}};
 
 static SPLASH_SHOWN: AtomicBool = AtomicBool::new(false);
 pub fn is_splash_shown() -> bool {
     SPLASH_SHOWN.load(atomic::Ordering::Acquire)
 }
 
-type ChangeViewFn = extern "C" fn(
-    this: *mut Il2CppObject, next_view_id: i32, view_info: *mut Il2CppObject,
-    callback_on_change_view_cancel: *mut Il2CppObject, callback_on_change_view_accept: *mut Il2CppObject,
-    force_change: bool
-);
-extern "C" fn ChangeView(
-    this: *mut Il2CppObject, next_view_id: i32, view_info: *mut Il2CppObject,
-    callback_on_change_view_cancel: *mut Il2CppObject, callback_on_change_view_accept: *mut Il2CppObject,
-    force_change: bool
-) {
-    get_orig_fn!(ChangeView, ChangeViewFn)(
-        this, next_view_id, view_info, callback_on_change_view_cancel, callback_on_change_view_accept, force_change
-    );
+fn ChangeViewCommon(next_view_id: i32) {
     if next_view_id == 1 { // ViewId.Splash
         SPLASH_SHOWN.store(true, atomic::Ordering::Release);
     }
 }
 
+type ChangeViewJpfn = extern "C" fn(
+    this: *mut Il2CppObject, next_view_id: i32, view_info: *mut Il2CppObject,
+    callback_on_change_view_cancel: *mut Il2CppObject, callback_on_change_view_accept: *mut Il2CppObject,
+    force_change: bool, is_fast_destroy: bool
+);
+extern "C" fn ChangeViewJp(
+    this: *mut Il2CppObject, next_view_id: i32, view_info: *mut Il2CppObject,
+    callback_on_change_view_cancel: *mut Il2CppObject, callback_on_change_view_accept: *mut Il2CppObject,
+    force_change: bool, is_fast_destroy: bool
+) {
+    get_orig_fn!(ChangeViewJp, ChangeViewJpfn)(
+        this, next_view_id, view_info, callback_on_change_view_cancel,
+        callback_on_change_view_accept, force_change, is_fast_destroy
+    );
+    ChangeViewCommon(next_view_id);
+}
+
+type ChangeViewOtherfn = extern "C" fn(
+    this: *mut Il2CppObject, next_view_id: i32, view_info: *mut Il2CppObject,
+    callback_on_change_view_cancel: *mut Il2CppObject, callback_on_change_view_accept: *mut Il2CppObject,
+    force_change: bool
+);
+extern "C" fn ChangeViewOther(
+    this: *mut Il2CppObject, next_view_id: i32, view_info: *mut Il2CppObject,
+    callback_on_change_view_cancel: *mut Il2CppObject, callback_on_change_view_accept: *mut Il2CppObject,
+    force_change: bool
+) {
+    get_orig_fn!(ChangeViewOther, ChangeViewOtherfn)(
+        this, next_view_id, view_info, callback_on_change_view_cancel,
+        callback_on_change_view_accept, force_change
+    );
+    ChangeViewCommon(next_view_id);
+}
+
 pub fn init(umamusume: *const Il2CppImage) {
     get_class_or_return!(umamusume, Gallop, SceneManager);
 
-    let ChangeView_addr = get_method_addr(SceneManager, c"ChangeView", 5);
-
-    new_hook!(ChangeView_addr, ChangeView);
+    if Hachimi::instance().game.region == Region::Japan {
+        let ChangeView_addr = get_method_addr(SceneManager, c"ChangeView", 6);
+        new_hook!(ChangeView_addr, ChangeViewJp);
+    }
+    else {
+        let ChangeView_addr = get_method_addr(SceneManager, c"ChangeView", 5);
+        new_hook!(ChangeView_addr, ChangeViewOther);
+    }
 }
