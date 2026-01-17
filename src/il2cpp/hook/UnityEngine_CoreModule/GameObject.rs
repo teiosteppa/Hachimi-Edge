edfd4703c18640e7f39e0d575d4427dada554146
teiosteppa <github@teiostep.com>
2026-01-17 14:39:46 -0800

    feat: try to modularize updates to gameobject components for portability

--- src/il2cpp/hook/UnityEngine_CoreModule/GameObject.rs
+++ src/il2cpp/hook/UnityEngine_CoreModule/GameObject.rs
@@ -4,7 +4,8 @@ use crate::{
     core::ext::Utf16StringExt,
     il2cpp::{
         api::il2cpp_resolve_icall, ext::Il2CppObjectExt, hook::{
-            Plugins::AnimateToUnity::AnRoot, UnityEngine_AssetBundleModule::AssetBundle, umamusume::FlashActionPlayer
+            Plugins::AnimateToUnity::AnRoot, UnityEngine_AssetBundleModule::AssetBundle,
+            umamusume::{CameraData::{self, ShadowResolution}, FlashActionPlayer}
         }, symbols::{Array, get_method_addr}, types::*
     }
 };
@@ -64,9 +65,23 @@ pub fn on_LoadAsset(bundle: *mut Il2CppObject, this: *mut Il2CppObject, name: &U
     }
 }
 
+fn customize(component: *mut Il2CppObject) {
+        match unsafe { (*component).klass() } {
+            // graphics quality - shadow resolution
+            CameraData if CameraData == CameraData::class() => {
+                CameraData::set_IsOverrideShadowResolution(component, true);
+                CameraData::set_OverrideShadowResolution(component, ShadowResolution::_4096);
+            }
+            _ => return
+        }
+}
+
 type Internal_AddComponentWithTypeFn = extern "C" fn(this: *mut Il2CppObject, componentType: *mut Il2CppType) -> *mut Il2CppObject;
 extern "C" fn Internal_AddComponentWithType(this: *mut Il2CppObject, componentType: *mut Il2CppType) -> *mut Il2CppObject {
     let component = get_orig_fn!(Internal_AddComponentWithType, Internal_AddComponentWithTypeFn)(this, componentType);
+    if !component.is_null() {
+        customize(component);
+    }
     component
 }
 
@@ -79,6 +94,12 @@ struct FastPath {
 type TryGetComponentFastPathFn = extern "C" fn(this: *mut Il2CppObject, type_: *mut Il2CppType, oneFurtherThanResultValue: usize);
 extern "C" fn TryGetComponentFastPath(this: *mut Il2CppObject, type_: *mut Il2CppType, oneFurtherThanResultValue: usize) {
     get_orig_fn!(TryGetComponentFastPath, TryGetComponentFastPathFn)(this, type_, oneFurtherThanResultValue);
+    let fastPath = (oneFurtherThanResultValue - std::mem::size_of::<*mut Il2CppObject>())
+        as *mut FastPath;
+    let component = unsafe { (*fastPath).component };
+    if !component.is_null() {
+        customize(component);
+    }
 }
 
 pub fn init(UnityEngine_CoreModule: *const Il2CppImage) {
