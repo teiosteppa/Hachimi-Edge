c7e7185a6361567e0dffe0237b26c4016dda1008
teiosteppa <github@teiostep.com>
2026-05-15 15:17:35 -0700

    Revert "feat: redirect stderr (rust/unity) to log file"
    This reverts commit d7786ec5a1b37935d65b8f375ec151c162befa21.

--- src/ios/main.rs
+++ src/ios/main.rs
@@ -1,46 +1,8 @@
 use crate::core::Hachimi;
 use super::hook;
 
-// sets RUST_BACKTRACE=full so Rust panics emit full backtraces, & redirects stderr to capture Rust/Unity log output
-// uses dup2 because spawning threads inside a ctor is unsafe on Apple platforms
-fn pre_init() {
-    unsafe {
-        libc::setenv(
-            c"RUST_BACKTRACE".as_ptr() as *const libc::c_char,
-            c"full".as_ptr() as *const libc::c_char,
-            1,
-        );
-    }
-
-    let log_path = super::utils::get_log_path();
-    if let Some(parent) = log_path.parent() {
-        std::fs::create_dir_all(parent).ok();
-    }
-    if let Some(path_str) = log_path.to_str() {
-        if let Ok(cpath) = std::ffi::CString::new(path_str) {
-            unsafe {
-                let fd = libc::open(
-                    cpath.as_ptr(),
-                    libc::O_WRONLY | libc::O_CREAT | libc::O_APPEND,
-                    0o644,
-                );
-                if fd >= 0 {
-                    libc::dup2(fd, libc::STDERR_FILENO);
-                    libc::close(fd);
-                }
-            }
-        }
-    }
-}
-
-/// iOS entry point via the `#[ctor]` constructor attribute.
-///
-/// This function is called by dyld **before** the app's `main()` runs,
-/// giving Hachimi a chance to install its hooks early.
 #[ctor::ctor]
 fn hachimi_ios_init() {
-    pre_init();
-
     if !Hachimi::init() {
         unsafe {
             libc::syslog(
