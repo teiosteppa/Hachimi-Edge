d5ff28883dca1add74d3905eb2ee7fa70140b0dd
teiosteppa <github@teiostep.com>
2026-05-15 15:44:22 -0700

    chore: remove unnecessary unsafe c imports

--- src/ios/hook.rs
+++ src/ios/hook.rs
@@ -1,27 +1,20 @@
+use mach2::{dyld, loader::mach_header};
 use crate::core::Hachimi;
 
-#[repr(C)]
-struct MachHeader {
-    _opaque: [u8; 0],
-}
-
+// not exposed by mach2 crate
 extern "C" {
-    fn _dyld_image_count() -> u32;
-    fn _dyld_get_image_header(image_index: u32) -> *const MachHeader;
-    fn _dyld_get_image_name(image_index: u32) -> *const libc::c_char;
-    fn _dyld_get_image_vmaddr_slide(image_index: u32) -> libc::intptr_t;
     fn _dyld_register_func_for_add_image(
-        func: Option<unsafe extern "C" fn(*const MachHeader, libc::intptr_t)>,
+        func: Option<unsafe extern "C" fn(*const mach_header, libc::intptr_t)>,
     );
 }
 
-unsafe extern "C" fn on_image_added(mh: *const MachHeader, slide: libc::intptr_t) {
-    let count = _dyld_image_count();
+unsafe extern "C" fn on_image_added(mh: *const mach_header, slide: libc::intptr_t) {
+    let count = dyld::_dyld_image_count();
     for i in 0..count {
-        let img_mh = _dyld_get_image_header(i);
+        let img_mh = dyld::_dyld_get_image_header(i);
         if img_mh != mh { continue; }
 
-        let raw = _dyld_get_image_name(i);
+        let raw = dyld::_dyld_get_image_name(i);
         if raw.is_null() { break; }
         let name = std::ffi::CStr::from_ptr(raw)
             .to_str()
